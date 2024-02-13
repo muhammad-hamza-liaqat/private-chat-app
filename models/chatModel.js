eeb6381eb0cb3d4339f9604f4a05de15914b9071
Muhammad Hamza <mh408800@gmail.com>
2024-02-13 11:23:49 +0500

    cleanUp

--- models/chatModel.js
+++ models/chatModel.js
@@ -1,8 +1,16 @@
 const mongoose = require("mongoose");
 
 const chatSchema = new mongoose.Schema({
-  participants: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
-  createdAt: { type: Date, default: Date.now },
+  participants: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+  ],
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
 });
 
 const chatModel = mongoose.model("Chat", chatSchema);
