ffdf930306c2869b8cea19d0d6c33dd90235ff67
Muhammad Hamza <mh408800@gmail.com>
2024-02-13 13:00:04 +0500

    bug fixed

--- controller/userController.js
+++ controller/userController.js
@@ -1,7 +1,6 @@
 const userModel = require("../models/userModel");
 const bcrypt = require("bcrypt");
 
-
 const registerMe = async (req, res) => {
   const { name, email, password } = req.body;
   if (!name || !email || !password) {
@@ -15,13 +14,11 @@ const registerMe = async (req, res) => {
       password: hashedPassword,
     });
     console.log("user created", newUser);
-    return res
-      .status(201)
-      .json({
-        message: "user created successfully!",
-        name: newUser.name,
-        email: newUser.email,
-      });
+    return res.status(201).json({
+      message: "user created successfully!",
+      name: newUser.name,
+      email: newUser.email,
+    });
   } catch (error) {
     console.log("error at registerMe controller", error);
     return res
@@ -30,41 +27,58 @@ const registerMe = async (req, res) => {
   }
 };
 
-const login = async (req,res)=>{
-    const {email, password} = req.body;
-    if (!email || !password){
-        return res.status(400).json({message: "all fields are required!"})
+const login = async (req, res) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    return res.status(400).json({ message: "all fields are required!" });
+  }
+  try {
+    const userToFind = await userModel.findOne({ email: email });
+    if (!userToFind) {
+      return res.status(404).json({ message: "invalid email or password" });
     }
-    try {
-        const userToFind = await userModel.findOne({email:email});
-        if (!userToFind){
-            return res.status(404).json({message:"invalid email or password"})
-        }
-        const comparePassword = await bcrypt.compare(password, userToFind.password);
-        if (!comparePassword){
-            return res.status(404).json({message: "invalid email or password"});
-        }
-        console.log(`${userToFind.email} login `);
-        // return res.status(200).json({message: "user login successfully!", user: userToFind.name, email: userToFind.email})
-        res.redirect('http://localhost:8080/api/user/private')
-        
-    } catch (error) {
-        console.log("error occured at login controller", error);
-        return res.status(500).json({message: "internal server error", error: error})
+    const comparePassword = await bcrypt.compare(password, userToFind.password);
+    if (!comparePassword) {
+      return res.status(404).json({ message: "invalid email or password" });
     }
-}
+    console.log(`${userToFind.email} login `);
+    // return res.status(200).json({message: "user login successfully!", user: userToFind.name, email: userToFind.email})
+    res.redirect("http://localhost:8080/api/user/private");
+  } catch (error) {
+    console.log("error occured at login controller", error);
+    return res
+      .status(500)
+      .json({ message: "internal server error", error: error });
+  }
+};
 
+const loginPage = (req, res) => {
+  res.render("login");
+};
 
+const registerPage = (req, res) => {
+  res.render("register");
+};
 
-const loginPage = (req,res)=>{
-  res.render('login');
-}
+const privateChatPage = (req, res) => {
+  res.render("privateChat");
+};
 
-const registerPage = (req,res)=>{
-  res.render('register');
-}
+const getUsers = async (req, res) => {
+  try {
+    const users = await userModel.find({}); // Assuming this fetches users from your database
+    res.json({ users }); // Return the users as JSON
+  } catch (error) {
+    console.error("Error fetching user data:", error);
+    res.status(500).json({ error: "Internal Server Error" }); // Return error response
+  }
+};
 
-const privateChatPage = (req,res)=>{
-  res.render('privateChat')
-}
-module.exports = { registerMe, login, loginPage, registerPage, privateChatPage };
+module.exports = {
+  registerMe,
+  login,
+  loginPage,
+  registerPage,
+  privateChatPage,
+  getUsers,
+};
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,11 +1,11 @@
 const express = require("express");
-const { registerMe, login, registerPage, loginPage, privateChatPage } = require("../controller/userController");
+const { registerMe, login, registerPage, loginPage, privateChatPage, getUsers } = require("../controller/userController");
 const userRoutes = express.Router();
 
 
 userRoutes.route("/register-me").get(registerPage).post(registerMe);
 userRoutes.route("/login-me").get(loginPage).post(login);
 userRoutes.route("/private").get(privateChatPage);
-
+userRoutes.route("/get-user").get(getUsers);
 
 module.exports = userRoutes
